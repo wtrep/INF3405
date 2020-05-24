8cbc386ab130344520e7cc219799fd30ddb35eee
Nathan Ramsay-Vejlens <nathanrv98@gmail.com>
2020-05-24 09:23:43 -0400

    Issue #1,8,11,12,14,15,16

--- TP1/Client/src/main/java/client/Client.java
+++ TP1/Client/src/main/java/client/Client.java
@@ -1,24 +1,193 @@
 package client;
 
-import java.io.DataInputStream;
 import java.net.Socket;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.PrintWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Client {
-    public static Socket socket;
+    private static Socket socket;
 
-    public static void main(String[] args) throws Exception {
-        String serverAddress = "127.0.0.1";
-        int port = 5000;
+    /*
+     * Application client
+     */
 
+    private static String validationIP(BufferedReader reader) {
+        //Demande IP
+        System.out.println("Veuillez entrez l'adresse IP du serveur : ");
+        //Initialisation de variable
+        String serverAddress = "";
+        boolean valid = false;
+        int octet1, octet2, octet3, octet4;
+
+        while (!valid) {
+            //Donnees entrée par la console
+            try {
+                serverAddress = reader.readLine();
+            }
+            catch(IOException e) {
+                e.printStackTrace();
+            }
+            //Vérification de la validité des données
+            int nbrDePoints = serverAddress.length()-serverAddress.replace(".", "").length();
+            if (serverAddress.contains(".") && nbrDePoints==3) {
+                String[] octets = serverAddress.split("\\."); // Les backslash sont utilises pour echapper valeur special de .
+                if(octets.length == 4) {
+                    try {
+                        octet1 = Integer.parseInt(octets[0]);
+                        octet2 = Integer.parseInt(octets[1]);
+                        octet3 = Integer.parseInt(octets[2]);
+                        octet4 = Integer.parseInt(octets[3]);
+                    }
+                    catch (NumberFormatException e)
+                    {
+                        octet1 = octet2 = octet3 = octet4 = 128;
+                    }
+
+                    if(octet1 >= 0 && octet1 < 128 ) {
+                        if(octet2 >= 0 && octet2 < 128 ) {
+                            if(octet3 >= 0 && octet3 < 128 ) {
+                                if(octet4 >= 0 && octet4 < 128 ) {
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            System.out.println("Adresse IP entrée invalide! Veuillez entre une adresse du format XXX.XXX.XXX.XXX : ");
+        }
+
+        return serverAddress;
+    }
+
+    private static int validationPort(BufferedReader reader) {
+        //Demande Port
+        System.out.println("Veuillez entrez le port d'écoute du serveur : ");
+        int serverPort = 0;
+        while( serverPort<5000 || serverPort>5050 )
+        {
+            //Donnees entrées par la console
+            try {
+                serverPort = Integer.parseInt(reader.readLine());
+            }
+            catch(Exception e) {
+                serverPort = 0;
+            }
+            if (serverPort>=5000 && serverPort<=5050) //Pour ne pas afficher message d'erreur si correct
+                break;
+            //Message d'erreur
+            System.out.println("Port invalide! (Veuillez entrez un nombre entre 5000 et 5050) ");
+        }
+
+        return serverPort;
+    }
+
+    private static class SendMessage extends Thread {
+        BufferedReader input;
+        BufferedWriter output;
+        boolean connected;
+
+        public SendMessage(Socket socket) {
+            input = new BufferedReader(new InputStreamReader(System.in));
+            connected = true;
+            try {
+                output = new BufferedWriter(new PrintWriter(socket.getOutputStream()));
+            } catch (IOException e) {
+                System.out.println("Le socket n'est pas connecté veuillez réessayer.");
+                connected = false;
+            }
+        }
+
+        public void run() {
+            String message;
+            String inputMessage;
+            while(true) {
+                try {
+                    //Chercher input
+                    inputMessage = input.readLine();
+
+                    //TODO Former message
+                    message = inputMessage;
+
+                    //Envoyer le message
+                    output.write(message);
+                    output.newLine();
+                    output.flush();
+                }
+                catch(IOException e) {
+                    System.out.println("Erreur dans l'envoi du message! Déconnexion.");
+                    connected = false;
+                }
+            }
+        }
+    }
+
+    private static class ReadMessage extends Thread {
+        BufferedReader input;
+        boolean connected;
+
+        public ReadMessage(Socket socket) {
+            connected = true;
+            try {
+                input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            } catch (IOException e) {
+                System.out.println("Le socket n'est pas connecté veuillez réessayer.");
+                connected = false;
+            }
+        }
+
+        public void run() {
+            String message;
+            while(connected) {
+                try {
+                    //Chercher le message
+                    message = input.readLine();
+
+                    //Afficher le message
+                    System.out.println(message);
+                }
+                catch(IOException e) {
+                    System.out.println("Erreur dans la réception du message! Déconnexion.");
+                    connected = false;
+                }
+            }
+        }
+    }
+
+
+    public static void main(String[] args) throws Exception
+    {
+        //Issue #11 : Saisie des paramètres du serveur (adresse IP, port d'écoute entre 5000 et 5050)
+        //Canal d'entree de la console
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        //Addresse et port du serveur
+        String serverAddress = validationIP(reader);
+        int port= validationPort(reader);
+//		int port = 5000;
+//		String serverAddress = "127.0.0.1";
+
+        //TEST pour receiveMessage(socket)
+        //Creation d'une nouvelle connexion avec le serveur
         socket = new Socket(serverAddress, port);
+//		BufferedReader serverReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        try {
+            //Écriture, reception
+            if (true) {
+                new SendMessage(socket).start();
+                new ReadMessage(socket).start();
+            }
+            while(true) {}
+        }
+        finally {
+            System.out.println("Tu as été déconnecté du serveur!");
 
-        System.out.format("The server is running on %s:%d%n", serverAddress, port);
-
-        DataInputStream in = new DataInputStream(socket.getInputStream());
-
-        String helloWorldFromServer = in.readUTF();
-        System.out.println(helloWorldFromServer);
-
-        socket.close();
+            //Fermeture de la connexion avec le serveur
+            socket.close();
+        }
     }
 }
+
--- TP1/Serveur/src/main/java/ca/polymtl/inf3405/server/Serveur.java
+++ TP1/Serveur/src/main/java/ca/polymtl/inf3405/server/Serveur.java
@@ -2,38 +2,215 @@ package ca.polymtl.inf3405.server;
 
 import ca.polymtl.inf3405.protocol.Message;
 
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.net.Socket;
-import java.util.Map;
-import java.util.Queue;
+import java.io.*;
+import java.net.*;
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
 
 public class Serveur {
     private volatile static Map<String, User> connectedClients;
     private volatile static Queue<Message> messagesQueue;
 
-    public static void main(String[] args) throws Exception {
-        
-    }
+    private static ServerSocket listener;
+    public static List<ClientHandler> clients;
 
-    private static class ClientHandler extends Thread {
-        private Socket socket;
-        private int clientNumber;
+    private static String validationIP(BufferedReader reader) {
+        //Demande IP
+        System.out.println("Veuillez entrez l'adresse IP du serveur : ");
+        //Initialisation de variable
+        String serverAddress = "";
+        boolean valid = false;
+        int octet1, octet2, octet3, octet4;
 
-        public ClientHandler(Socket socket, int clientNumber) {
-            this.socket = socket;
-            this.clientNumber = clientNumber;
-            System.out.println("New connection with client#" + clientNumber + " at" + socket);
-        }
-
-        public void run() {
+        while (!valid) {
+            //Donnees entrée par la console
             try {
-                DataOutputStream out = new DataOutputStream(socket.getOutputStream());
-                out.writeUTF("Hello from server - you are client#" + clientNumber);
-            } catch (IOException e) {
-                System.out.println("Couldn't close a socket, what's going on?");
+                serverAddress = reader.readLine();
             }
-            System.out.println("Connection with client# " + clientNumber + " closed");
+            catch(IOException e) {
+                e.printStackTrace();
+            }
+            //Vérification de la validité des données
+            int nbrDePoints = serverAddress.length()-serverAddress.replace(".", "").length();
+            if (serverAddress.contains(".") && nbrDePoints==3) {
+                String[] octets = serverAddress.split("\\."); // Les backslash sont utilises pour echapper valeur special de .
+                if(octets.length == 4) {
+                    try {
+                        octet1 = Integer.parseInt(octets[0]);
+                        octet2 = Integer.parseInt(octets[1]);
+                        octet3 = Integer.parseInt(octets[2]);
+                        octet4 = Integer.parseInt(octets[3]);
+                    }
+                    catch (NumberFormatException e)
+                    {
+                        octet1 = octet2 = octet3 = octet4 = 128;
+                    }
+
+                    if(octet1 >= 0 && octet1 < 128 ) {
+                        if(octet2 >= 0 && octet2 < 128 ) {
+                            if(octet3 >= 0 && octet3 < 128 ) {
+                                if(octet4 >= 0 && octet4 < 128 ) {
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            System.out.println("Adresse IP entrée invalide! Veuillez entre une adresse du format XXX.XXX.XXX.XXX : ");
+        }
+
+        return serverAddress;
+    }
+
+    private static int validationPort(BufferedReader reader) {
+        //Demande Port
+        System.out.println("Veuillez entrez le port d'écoute du serveur : ");
+        int serverPort = 0;
+        while( serverPort<5000 || serverPort>5050 )
+        {
+            //Donnees entrées par la console
+            try {
+                serverPort = Integer.parseInt(reader.readLine());
+            }
+            catch(Exception e) {
+                serverPort = 0;
+            }
+            if (serverPort>=5000 && serverPort<=5050) //Pour ne pas afficher message d'erreur si correct
+                break;
+            //Message d'erreur
+            System.out.println("Port invalide! (Veuillez entrez un nombre entre 5000 et 5050) ");
+        }
+
+        return serverPort;
+    }
+
+    public static void main(String[] args) throws Exception
+    {
+        //Canal d'entree de la console
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        //Validation adresse IP
+        String serverAddress = validationIP(reader);
+        //Validation port
+        int serverPort= validationPort(reader);
+//		String serverAddress = "127.0.0.1";
+//		int serverPort = 5000;
+
+        String username ="username";
+
+        listener = new ServerSocket();
+        listener.setReuseAddress(true);
+        InetAddress serverIP = InetAddress.getByName(serverAddress);
+
+        //Association de l'adresse et du port a la connexion
+        listener.bind(new InetSocketAddress(serverIP, serverPort));
+
+        System.out.format("The server is running on %s:%d%n", serverAddress, serverPort);
+
+
+        try
+        {
+            /*
+             * A chaque fois qu'un nouveau client se connecte, on execute la fonction
+             * Run() de l'objet ClientHandler
+             */
+            clients = new ArrayList<>();
+            while(true)
+            {
+                //Important: la fonction accept() est bloquante : attend qu'un prochain client se connecte
+                // Une nouvelle connexion
+                ClientHandler client = new ClientHandler(listener.accept(), username);
+                clients.add(client);
+                System.out.println("Client added");
+                client.start();
+            }
+        }
+        finally
+        {
+            //Fermeture du socket d'écoute
+            listener.close();
         }
     }
+
+
+    /*
+     * Un thread qui se charge de traiter la demande de chaque client
+     * sur un socket particulier
+     */
+    private static class ClientHandler extends Thread
+    {
+        private Socket socket;
+        private String username;
+        private BufferedReader reader;
+        private BufferedWriter writer;
+        boolean isLoggedIn;
+
+        public ClientHandler(Socket socket,String username)
+        {
+            this.socket = socket;
+            this.username = username;
+            System.out.println("New connection with " + username + " at " + socket);
+            isLoggedIn=true;
+            try {
+                this.reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                this.writer = new BufferedWriter(new PrintWriter(socket.getOutputStream()));
+            } catch (IOException e) {
+                System.out.println("Erreur lors de la connexion, veuillez réessayer!");
+                isLoggedIn=false;
+            }
+        }
+
+        /*
+         * Un thread qui se charge d'un client.
+         */
+        public void run()
+        {
+            String received;
+            DateTimeFormatter timeFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd k:m:s");
+            while(isLoggedIn) {
+                try {
+                    //Recevoir messages
+                    received = reader.readLine();
+
+                    //TODO Transforme received into message
+                    String message = "["+username+" - "+"IPaddress:Port"+" - "+LocalDateTime.now().format(timeFormat)+"]:"+received;
+
+                    //Afficher sur console serveur
+                    System.out.println(message);
+
+                    //Pour tous les clients, TODO :ajouter filtre par après
+                    for(ClientHandler client : Server.clients) {
+                        client.getOutput().write(message);
+                        client.getOutput().newLine();
+                        client.getOutput().flush();
+                    }
+
+                    if(received == "logout") {
+                        isLoggedIn=false;
+                    }
+
+
+
+                } catch (Exception e) {
+                    System.out.println("La connexion a été coupée avec " + this.username + "!");
+                    isLoggedIn=false;
+                    clients.remove(this);
+                }
+            }
+            try {
+                //Relâcher les ressources
+                this.reader.close();
+                this.writer.close();
+                this.socket.close();
+            } catch (IOException e ){
+                System.out.println("Erreur dans la fermeture des buffers!");
+            }
+        }
+
+        private BufferedWriter getOutput() {
+            return writer;
+        }
+    }
+
 }
