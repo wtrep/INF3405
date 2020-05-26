9ae3b3dee7e4f3bfce93a4ad609d7eaa743dfe78
William Trépanier <wil.trepanier@gmail.com>
2020-05-26 15:19:46 -0400

    added ip and port to the connected client

--- TP1/Serveur/src/main/java/ca/polymtl/inf3405/server/ConnectedUser.java
+++ TP1/Serveur/src/main/java/ca/polymtl/inf3405/server/ConnectedUser.java
@@ -1,17 +1,22 @@
 package ca.polymtl.inf3405.server;
 
+import java.net.InetAddress;
 import java.nio.charset.StandardCharsets;
 import java.security.SecureRandom;
 import java.time.Instant;
 
 public class ConnectedUser extends User {
-    public final String token;
-    public volatile Instant lastConnection;
+    private final String token;
+    private volatile Instant lastConnection;
+    private final InetAddress userAddress;
+    private final int userPort;
 
-    public ConnectedUser(User user) {
+    public ConnectedUser(User user, InetAddress addr, int port) {
         super(user);
         token = generateToken();
         lastConnection = Instant.now();
+        userAddress = addr;
+        userPort = port;
     }
 
     public String getToken() {
@@ -20,6 +25,14 @@ public class ConnectedUser extends User {
 
     public Instant getLastConnection() {
         return lastConnection;
+    }
+
+    public InetAddress getUserAddress() {
+        return userAddress;
+    }
+
+    public int getUserPort() {
+        return userPort;
     }
 
     public synchronized void updateLastConnection() {
